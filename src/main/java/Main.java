924acdc6215c7834f32425aa5c682eaf50d7c61a
kartikgupta <kartik.gupta2505@gmail.com>
2025-08-11 23:04:24 +0530

    returning correct correlation id as request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.io.InputStream;
 
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    // 
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     int port = 9092;
@@ -19,7 +20,24 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
-      clientSocket.getOutputStream().write(new byte[] {00, 00, 00, 00, 00, 00, 00, 07} );
+
+      InputStream in = new BufferedInputStream(clientSocket.getInputStream());
+      
+      byte[] messageSizeBytes = in.readNBytes(4);
+      System.out.println(messageSizeBytes);
+
+      int messageSize = ByteBuffer.wrap(messageSizeBytes).getInt();
+
+      byte[] apiKey = in.readNBytes(2);
+      byte[] apiVersion = in.readNBytes(2);
+      int correlationId = ByteBuffer.wrap(in.readNBytes(4)).getInt();
+
+      clientSocket.getOutputStream().write(messageSizeBytes);
+      var res = ByteBuffer.allocate(4).putInt(correlationId).array();
+
+      clientSocket.getOutputStream().write(res);
+
+      // clientSocket.getOutputStream().write(new byte[] {00, 00, 00, 00, 00, 00, 00, 07} );
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
